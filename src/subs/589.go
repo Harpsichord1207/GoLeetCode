28088f8017d72e9f6dc2512c50a395274324d479
Derek <liutao3@cig.com.cn>
2021-07-06 21:56:41 +0800

    finish 589

---
+++ src/subs/589.go
@@ -0,0 +1,18 @@
+package subs
+
+type Node struct {
+	Val int
+	Children []*Node
+}
+
+func preorder(root *Node) []int {
+	var output []int
+	if root == nil {
+		return output
+	}
+	output = append(output, root.Val)
+	for _, child := range root.Children {
+		output = append(output, preorder(child)...)
+	}
+	return output
+}
