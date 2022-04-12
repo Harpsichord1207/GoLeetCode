91eead7b5a13b14198386d18eed544489718ac6b
Harpsichord <feelingsxxx@gmail.com>
2022-04-12 09:04:15 +0800

    finish 583

--- subs/583.go
+++ subs/583.go
@@ -1,8 +1,6 @@
 package subs
 
-import "fmt"
-
-func min72(a int, b int, c int) int {
+func min583(a int, b int, c int) int {
 	if a <= b && a <= c {
 		return a
 	}
@@ -12,7 +10,12 @@ func min72(a int, b int, c int) int {
 	return c
 }
 
-func minDistance(word1 string, word2 string) int {
+func minDistance583(word1 string, word2 string) int {
+	// dp[i][j] 表示word1[:i] 与 word2[:j]的距离
+	// 状态转移方程:
+	// dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+2*int(word1[i-1]!=word2[j-1]))
+	// 基本和72题是一样的
+
 	l1 := len(word1)
 	l2 := len(word2)
 
@@ -24,10 +27,6 @@ func minDistance(word1 string, word2 string) int {
 		}
 		dp[i][0] = i
 	}
-
-	// dp[i][j] 表示word1[:i] 与 word2[:j]的距离
-	// 状态转移方程:
-	// dp[i][j] = min(dp[i][j-1]+1, dp[i-1][j]+1, dp[i-1][j-1]+int(word1[i-1]!=word2[j-1]))
 
 	for j := 0; j <= l2; j++ {
 		dp[0][j] = j
@@ -41,15 +40,15 @@ func minDistance(word1 string, word2 string) int {
 			if word1[i-1] == word2[j-1] {
 				t = dp[i-1][j-1]
 			} else {
-				t = dp[i-1][j-1] + 1
+				t = dp[i-1][j-1] + 2
 			}
-			dp[i][j] = min72(r, s, t)
+			dp[i][j] = min583(r, s, t)
 		}
 	}
 	// fmt.Println(dp)
 	return dp[l1][l2]
 }
 
-func Test72() {
-	fmt.Println(minDistance("horse", "ros"))
+func Test583() {
+
 }
