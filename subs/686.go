c17bd83df94a6d421735d2681c395701fce95cbb
Harpsichord <feelingsxxx@gmail.com>
2022-07-14 17:44:03 +0800

    686 优化速度

--- subs/686.go
+++ subs/686.go
@@ -5,24 +5,31 @@ import (
 	"strings"
 )
 
-// 莫名其妙
+// 100% beat!
 func repeatedStringMatch(a string, b string) int {
-	var r = 1
-	var ar = a
-	for {
-		if strings.Contains(ar, b) {
-			return r
-		} else if len(ar) > len(b) && !strings.Contains(ar+ar, b) {
-			return -1
-		}
-		r += 1
-		ar += a
-
+	la := len(a)
+	lb := len(b)
+	c := lb / la
+	if lb%la != 0 {
+		c++
 	}
+
+	ar := strings.Repeat(a, c)
+
+	if strings.Contains(ar, b) {
+		return c
+	}
+
+	// why here ?
+	if strings.Contains(ar+a, b) {
+		return c + 1
+	}
+
+	return -1
 }
 
 func Test686() {
-	a := "abcd"
-	b := "abcdb"
+	a := "a"
+	b := "aa"
 	fmt.Println(repeatedStringMatch(a, b))
 }
