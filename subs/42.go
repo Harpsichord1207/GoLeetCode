3f4c961d399a1eb61e6fcb7b82788f33942a7161
Derek <liutao3@cig.com.cn>
2023-06-01 17:08:07 +0800

    feat: finish 42 with brute fore + memo

--- subs/42.go
+++ subs/42.go
@@ -3,30 +3,40 @@ package subs
 import "fmt"
 
 func trap(height []int) int {
-	i := 0
-	j := len(height) - 1
+	l := len(height)
 	r := 0
-	for ; i < j; i++ {
-		leftHigh := 0
-		rightHigh := 0
-		for ii := 0; ii <= i; ii++ {
-			if height[ii] > leftHigh {
-				leftHigh = height[ii]
-			}
-		}
-		for jj := i; jj <= j; jj++ {
-			if height[jj] > rightHigh {
-				rightHigh = height[jj]
-			}
-		}
-		if leftHigh > rightHigh {
-			r += rightHigh
+	if l == 0 {
+		return 0
+	}
 
+	leftHighArr := make([]int, l) // leftHighArr[i]表示i左边最高的柱子
+	rightHighArr := make([]int, l)
+
+	for p := 0; p < len(height); p++ {
+		if p == 0 || height[p] > leftHighArr[p-1] {
+			leftHighArr[p] = height[p]
 		} else {
-			r += leftHigh
+			leftHighArr[p] = leftHighArr[p-1]
+		}
+	}
+
+	for p := l - 1; p >= 0; p-- {
+		if p == l-1 || height[p] > rightHighArr[p+1] {
+			rightHighArr[p] = height[p]
+		} else {
+			rightHighArr[p] = rightHighArr[p+1]
+		}
+	}
+
+	for i := 0; i < l; i++ {
+		if leftHighArr[i] > rightHighArr[i] {
+			r += rightHighArr[i]
+		} else {
+			r += leftHighArr[i]
 		}
 		r -= height[i]
 	}
+
 	return r
 }
 
