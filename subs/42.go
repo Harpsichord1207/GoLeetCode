2f2b45acf4fe17eee7478850d574a732f6eb8d56
Derek <liutao3@cig.com.cn>
2023-06-06 10:56:27 +0800

    feat: finish 42 with double pointer O(1)

--- subs/42.go
+++ subs/42.go
@@ -3,40 +3,34 @@ package subs
 import "fmt"
 
 func trap(height []int) int {
-	l := len(height)
 	r := 0
-	if l == 0 {
-		return 0
+	right := len(height) - 1
+	left := 0
+
+	if right < 0 {
+		return r
 	}
 
-	leftHighArr := make([]int, l) // leftHighArr[i]表示i左边最高的柱子
-	rightHighArr := make([]int, l)
+	rightMax := height[right]
+	leftMax := height[left]
 
-	for p := 0; p < len(height); p++ {
-		if p == 0 || height[p] > leftHighArr[p-1] {
-			leftHighArr[p] = height[p]
+	for left < right {
+		if height[left] < height[right] {
+			if height[left] > leftMax {
+				leftMax = height[left]
+			} else {
+				r += leftMax - height[left]
+			}
+			left += 1
 		} else {
-			leftHighArr[p] = leftHighArr[p-1]
+			if height[right] > rightMax {
+				rightMax = height[right]
+			} else {
+				r += rightMax - height[right]
+			}
+			right -= 1
 		}
 	}
-
-	for p := l - 1; p >= 0; p-- {
-		if p == l-1 || height[p] > rightHighArr[p+1] {
-			rightHighArr[p] = height[p]
-		} else {
-			rightHighArr[p] = rightHighArr[p+1]
-		}
-	}
-
-	for i := 0; i < l; i++ {
-		if leftHighArr[i] > rightHighArr[i] {
-			r += rightHighArr[i]
-		} else {
-			r += leftHighArr[i]
-		}
-		r -= height[i]
-	}
-
 	return r
 }
 
